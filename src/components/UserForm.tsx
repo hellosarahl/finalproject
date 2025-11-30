b74647d812444a221dc63f1e1d349e4d10318607
lr0826 <lr0826@bu.edu>
2025-11-30 15:49:13 -0500

    create components files, deleted one css file, and installed styled components

---
+++ src/components/UserForm.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+const UserFormContainer = styled.section`
+  margin: 0 auto;
+`;
+
+export function UserForm() {
+    return <UserFormContainer>User form goes here</UserFormContainer>;
+}
